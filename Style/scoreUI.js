1f67f8dc9dcb3641d94745abe56447ad1182655b
Lian <markliandperez61@gmail.com>
2024-04-12 21:09:40 +0800

    add some feature

--- Style/scoreUI.js
+++ Style/scoreUI.js
@@ -1,9 +1,10 @@
 import {StyleSheet} from 'react-native';
+import { back } from 'react-native/Libraries/Animated/Easing';
 
 const styles = StyleSheet.create({
   title: {
     color: '#fff',
-
+     
     fontSize: 50,
     fontWeight: 'bold',
     textAlign: 'center',
@@ -19,8 +20,8 @@ const styles = StyleSheet.create({
     opacity: 0.1,
   },
   container: {
-    height: 360,
-    width: 350,
+    height: 460,
+    width: 400,
     backgroundColor: '#808088',
     borderRadius: 10,
     borderWidth: 10,
@@ -30,37 +31,38 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.8,
     shadowRadius: 4,
     elevation: 5,
-    left: 40,
+    left: 14,
     top: 100,
   },
   player: {
     color: '#fff', // White
     fontSize: 18,
-    top: -230,
-
+    top: 20,
+    left: 20,
     fontWeight: 'bold',
   },
   round: {
     fontSize: 18,
     color: '#fff',
-    top: -210,
+    top: 20,
+   left: 20,
     fontWeight: 'bold',
   },
   scorebox: {
     borderRadius: 10,
-
-    top: -360,
+    top: -20,
+    left: -20,
   },
   score: {
-    textAlign: 'center',
+    textAlign: 'right',
     color: '#fff',
-    fontSize: 50,
+    fontSize: 30,
   },
   button:{
      width: 150,
      height: 50,
      borderRadius: 10,
-     left: 80,
+     left: 90,
      backgroundColor: "#fff",
      top: -240
   },
@@ -69,7 +71,73 @@ const styles = StyleSheet.create({
     textAlign: "center",
     top: 13 ,
     color: "#000"
-  }
+  },
+  Pcontainer: {
+    width: 360,
+    height: 90,
+    top: -420,
+    left: 9,
+    marginBottom: 10,
+    borderRadius: 10, // Border radius for rounded corners
+    shadowColor: '#000', // Shadow color
+    shadowOffset: {
+     
+    },
+    shadowOpacity: 0.25, // Opacity of the shadow
+    shadowRadius: 3.84, // Radius of the shadow
+    elevation: 4, // Android only - elevation for shadow effect
+  },
+  //MOdalUi next page
+  modalPlayerName:{
+    color: "#fff",
+    fontSize: 20,
+    top:-105,
+    left: 10,
+  },
+  modalRound:{
+    color: "#fff",
+    top:-100,
+    left: 10,
+    fontSize: 20
+  },
+  modalScore:{
+    color: "#fff",
+    fontSize: 40,
+    top:-120,
+    textAlign:"center"
+  },
+  modalStyles:{
+    width:"100%",
+    top:-300
+  },
+  modalTitle:{
+    color: "#fff",
+    textAlign:"center",
+    fontSize: 20,
+    top:-70
+  },
+  otherplayer:{
+    color:"#fff",
+    fontSize: 15,
+    textAlign:"center",
+    marginBottom: 10,
+    top:-60
+  },
+  Modalbutton:{
+    width: 150,
+    height: 50,
+    borderRadius: 10,
+    left: 120,
+    backgroundColor: "#fff",
+    top: -350
+
+  },
+  ModalbuttonText:{
+    fontSize: 20,
+    textAlign: "center",
+    top: 13 ,
+    color: "#000"
+  },
 });
 
 export default styles;
--- scorepanelUI/Scoring.js
+++ scorepanelUI/Scoring.js
@@ -1,49 +1,87 @@
-import React, {useEffect, useState} from 'react';
-import {View, SafeAreaView, Text, Image, TouchableOpacity} from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, SafeAreaView, Text, Image, TouchableOpacity, Modal, Button } from 'react-native';
 import styles from '../Style/scoreUI';
 import playersData from '../computation/datascore'; // Import player data
-import {computeScores} from '../computation/computation'; // Import score computation function
+import { computeScores } from '../computation/computation'; // Import score computation function
 
 const Score = () => {
   const img = require('../images/spade.png');
-  const [highestScorePlayer, setHighestScorePlayer] = useState(null);
+  const [playersWithScores, setPlayersWithScores] = useState([]);
+  const [selectedPlayer, setSelectedPlayer] = useState(null);
+  const [modalVisible, setModalVisible] = useState(false);
 
   useEffect(() => {
     // Calculate scores for players
     const updatedPlayersData = computeScores(playersData);
 
-    // Find the player with the highest score
-    const winner = updatedPlayersData.reduce((prevPlayer, currentPlayer) => {
-      return currentPlayer.score > prevPlayer.score
-        ? currentPlayer
-        : prevPlayer;
-    });
-
-    setHighestScorePlayer(winner);
+    // Set players with scores
+    setPlayersWithScores(updatedPlayersData);
   }, []);
+
+  const handleBoxPress = (player) => {
+    setSelectedPlayer(player);
+    setModalVisible(true);
+  };
+
+  const handleCloseModal = () => {
+    setModalVisible(false);
+    setSelectedPlayer(null);
+  };
 
   return (
     <SafeAreaView style={styles.BG}>
       <View style={styles.container}>
         <Image source={img} style={styles.spade} />
-        <Text style={styles.title}>Score</Text>
 
-        {highestScorePlayer && (
-          <View key={highestScorePlayer.id}>
-            <Text style={styles.player}>
-              Player Name: {highestScorePlayer.nickname}
-            </Text>
-            <Text style={styles.round}>
-              Round: {highestScorePlayer.roundCount}
-            </Text>
-            <View style={styles.scorebox}>
-              <Text style={styles.score}> {highestScorePlayer.score}</Text>
+        <View style={styles.boxContainer}>
+          {playersWithScores.slice(0, 4).map((player, index) => (
+            <TouchableOpacity key={player.id} onPress={() => handleBoxPress(player)} style={styles.Pcontainer}>
+              <View style={styles.box}>
+                <Text style={styles.player}>
+                  Player Name: {player.nickname}
+                </Text>
+                <Text style={styles.round}>
+                  Round: {player.roundCount}
+                </Text>
+                <View style={styles.scorebox}>
+                  <Text style={styles.score}> {player.score}</Text>
+                </View>
+              </View>
+            </TouchableOpacity>
+          ))}
+        </View>
+
+        <Modal
+          animationType="fade"
+          transparent={true}
+          visible={modalVisible}
+          onRequestClose={handleCloseModal}
+        >
+          <View style={styles.container}>
+          <Image source={img} style={styles.spade} />
+
+            <View style={styles.modalContent}>
+              {selectedPlayer && (
+                <View style = {styles.modalStyles}>           
+                  <Text style={styles.modalScore}>{selectedPlayer.score}</Text>
+                  <Text style = {styles.modalScore}>Score</Text>
+                  <Text style={styles.modalPlayerName}>Player Name: {selectedPlayer.nickname} </Text>
+                  <Text style={styles.modalRound}>Round: {selectedPlayer.roundCount}</Text>
+                  
+                  <Text style={styles.modalTitle}>Scores of Other Players:</Text>
+                  {playersWithScores.map((player, index) => (
+                    <View key={player.id}>
+                      <Text style = {styles.otherplayer}>{player.nickname}: {player.score}</Text>
+                    </View>
+                  ))}
+                </View>
+              )}
+                 <TouchableOpacity onPress={handleCloseModal}  style = {styles.Modalbutton}> 
+                  <Text style={styles.ModalbuttonText}>Close</Text>
+                  </TouchableOpacity>
             </View>
           </View>
-        )}
-        <TouchableOpacity  style={styles.button}>
-          <Text style={styles.buttonText}>Back</Text>
-        </TouchableOpacity>
+        </Modal>
       </View>
     </SafeAreaView>
   );
